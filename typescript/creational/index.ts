75ec7ae434b94f24e60a59cf63f7fb8de11e0fb3
mint.wzy <mint.wzy@gmail.com>
2022-09-20 13:54:09 +0800

    begin to add builder pattern

---
+++ typescript/creational/index.ts
@@ -0,0 +1,7 @@
+/*
+Creational design patterns abstract the instantiation process. They help make a system independent of how its objects
+are created, composed, and represented. A class creational pattern uses inheritance to vary the class that’s instantiated,
+whereas an object creational pattern will delegate instantiation to another object.
+
+
+ */
